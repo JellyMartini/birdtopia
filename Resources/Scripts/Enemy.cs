47b13dcac407578c89f5d73812f80f7e89d96806
JellyMartini <joel.martin02@bigpond.com>
2024-05-23 16:42:06 +0930

    saving for the night

--- Resources/Scripts/Enemy.cs
+++ Resources/Scripts/Enemy.cs
@@ -8,24 +8,47 @@ public class Enemy : MonoBehaviour
 {
     public NavMeshAgent agent;
     public Player player;
-    private Animator animator;
+    private Animator swordAnim;
+    private int tick, tick_max;
+    public float tick_interval, hitDistance, hitRadius;
+    private LayerMask playerLayerMask = 6;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-        animator = GetComponentsInChildren<Animator>()[0];
+        swordAnim = GetComponentsInChildren<Animator>()[1];
 
-        int i = 0;
-        while (!animator.name.Equals("sword") && GetComponentsInChildren<Animator>()[i] != null)
-        {
-            animator = GetComponentsInChildren<Animator>()[i++];
-        }
+        tick = -1;
+        tick_max = (int) (tick_interval / Time.fixedDeltaTime);
     }
 
     // Update is called once per frame
-    void Update()
-    {   
-        agent.destination = player.transform.position;
+    void FixedUpdate()
+    {
+        tick = (tick + 1) % tick_max;
+        if (tick == 0) agent.destination = player.transform.position;
+
+        if ((swordAnim.GetCurrentAnimatorStateInfo(0).normalizedTime % 1.0f > 0.1f && swordAnim.GetCurrentAnimatorStateInfo(0).normalizedTime % 1.0f < 0.15f) ||
+           (swordAnim.GetCurrentAnimatorStateInfo(0).normalizedTime % 1.0f > 0.6f && swordAnim.GetCurrentAnimatorStateInfo(0).normalizedTime % 1.0f < 0.65f))
+            if (ShootRay()) Debug.Log("Hit");
     }
+
+    private bool ShootRay()
+    {
+        if (Physics.SphereCast(transform.position, hitRadius, transform.TransformDirection(Vector3.forward), out RaycastHit sphereHit, hitDistance, ~playerLayerMask))
+        {
+            Debug.DrawLine(transform.position, transform.position + transform.TransformDirection(Vector3.forward) * hitDistance, Color.green);
+            return true;
+        }
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit lineHit, hitDistance, playerLayerMask))
+        {
+            Debug.DrawLine(transform.position, transform.position + transform.TransformDirection(Vector3.forward) * hitDistance, Color.green);
+            return true;
+        }
+        Debug.DrawLine(transform.position, transform.position + transform.TransformDirection(Vector3.forward) * hitDistance, Color.red, 1.0f);
+        return false;
+    }
+
+    
 }
--- Resources/Scripts/Player.cs
+++ Resources/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     public CharacterController controller;
     private GameObject mainCam;
+    private GameObject ViewModel;
 
     public float MoveSpeed { get; set; }
     public float JumpSpeed { get; set; }
@@ -15,14 +16,15 @@ public class Player : MonoBehaviour
     void Awake()
     {
         MoveSpeed = 10.0f;
-        JumpSpeed = 5.0f;
-        Gravity = -10.0f;
+        JumpSpeed = 10.0f;
+        Gravity = -15.0f;
         VerticalAcceleration = 0.0f;
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        ViewModel = GameObject.Find("ViewModel");
         controller = GetComponent<CharacterController>();
         mainCam = GameObject.FindGameObjectWithTag("MainCamera");
     }
@@ -32,15 +34,20 @@ public class Player : MonoBehaviour
         float CamY = mainCam.transform.rotation.eulerAngles.y;
         float CamY_Sin = Mathf.Sin(CamY * Mathf.Deg2Rad);
         float CamY_Cos = Mathf.Cos(CamY * Mathf.Deg2Rad);
+        float PlayerRotation = 0.0f;
+
+        Vector2 PlayerInputs = new Vector2(Input.GetAxis("Player_Horizontal"), Input.GetAxis("Player_Vertical"));
+        
+        if (PlayerInputs.magnitude > 1.0f) PlayerInputs.Normalize();
 
         Vector3 velocity = new Vector3(
-            Input.GetAxis("Player_Vertical") * CamY_Sin + Input.GetAxis("Player_Horizontal") * CamY_Cos,
+            PlayerInputs.y * CamY_Sin + PlayerInputs.x * CamY_Cos,
             0.0f,
-            Input.GetAxis("Player_Vertical") * CamY_Cos - Input.GetAxis("Player_Horizontal") * CamY_Sin
+            PlayerInputs.y * CamY_Cos - PlayerInputs.x * CamY_Sin
         );
 
-        if (velocity.magnitude > 1.0f) velocity.Normalize();
-
+        ViewModel.transform.rotation = Quaternion.Euler(0.0f, PlayerRotation, 0.0f);
+        
         velocity *= MoveSpeed;
 
         if (controller.isGrounded) VerticalAcceleration = Input.GetAxis("Player_Jump") * JumpSpeed;
